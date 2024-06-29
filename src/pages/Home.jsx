e675765c3f24684ea5004ac646aa33298d8de9a4
thisisrai <thisisrailee@gmail.com>
2024-06-29 08:56:01 -0700

    update home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = (props) => {
-  return <h1>Home</h1>
-}
+  return (
+    <div className="home-container">
+      <p>
+        Welcome to our beta application for job searching. By creating an account, you agree to not hold the application and its owner liable for any issues.
+      </p>
+    </div>
+  );
+};
 
 export default Home;
