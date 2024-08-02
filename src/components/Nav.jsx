98da193446c6f7408745e47a6859998d62ceb015
thisisrai <thisisrailee@gmail.com>
2024-08-01 19:19:38 -0700

    update styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,24 +3,32 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAppState } from "../AppState.jsx";
 
 const Nav = (props) => {
-  const { state, dispatch } = useAppState()
-  const navigate = useNavigate()
+  const { state, dispatch } = useAppState();
+  const navigate = useNavigate();
 
-  return <header>
-    <h1>Coffee Job</h1>
-    <nav>
-      {state.token ? <div onClick={() => {
-        dispatch({ type: "logout"});
-        navigate("/")
-      }}>Logout</div> :
-        <>
-          <Link to="/"><div>Home</div></Link>
-          <Link to="/auth/signup"><div>Signup</div></Link>
-          <Link to="/auth/login"><div>Login</div></Link>
-        </>
-      }
-    </nav>
-  </header>
-}
+  return (
+    <header>
+      <h1>Coffee Job</h1>
+      <nav>
+        {state.token ? (
+          <div
+            onClick={() => {
+              dispatch({ type: "logout" });
+              navigate("/");
+            }}
+          >
+            Logout
+          </div>
+        ) : (
+          <>
+            <Link to="/"><div>Home</div></Link>
+            <Link to="/auth/signup"><div>Signup</div></Link>
+            <Link to="/auth/login"><div>Login</div></Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
 
 export default Nav;
