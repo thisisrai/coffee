b9d8f7953e733243f0e6b09434979a343de65dee
Kavin Tjhan <tjhan.kavin@gmail.com>
2024-09-21 12:21:58 -0700

    feature #2: spruced up the background video on home page, fixed html/css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <video controls>
-        <source src="/coffeejob.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
       <div className="home-container">
+        <div className="border-blur"></div>
         <p>
           Welcome to our beta job search tracker application! By creating an
           account, you acknowledge that the application and its owner are not
           responsible for any issues that may arise.
         </p>
+        <video
+          className="background-video"
+          src="/coffeejob.mp4"
+          autoPlay
+          loop
+          muted
+        ></video>
       </div>
     </div>
   );
