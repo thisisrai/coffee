11899d9a1cb9b6424200b16bd5d1aca9be2e8dcb
thisisrai <thisisrailee@gmail.com>
2024-10-31 14:02:34 -0700

    add jobs when signed out

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -46,6 +46,9 @@ const Nav = (props) => {
                 <Link to="/">
                   <div>Home</div>
                 </Link>
+                <Link to="/job-listings">
+                  <div>Jobs</div>
+                </Link>
                 <Link to="/inspiration">
                   <div>Inspiration</div>
                 </Link>
