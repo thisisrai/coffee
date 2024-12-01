d64544a5ee70e2c449eb5021426f8b71f5f152c9
Kavin Tjhan <tjhan.kavin@gmail.com>
2024-11-30 21:27:26 -0800

    fix: routing issues fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,42 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = (env) => {
-  const isProduction = env.production === true;
-
-  return {
-    entry: "./src/index.jsx",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.[contenthash].js",
-      publicPath: "/"
-    },
-    mode: isProduction ? "production" : "development",
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    resolve: {
-      extensions: [".js", ".jsx"]
-    },
-    devServer: {
-      historyApiFallback: true,
-      hot: true,
-      port: process.env.PORT || 3000,
-      static: {
-        directory: path.join(__dirname, "public")
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./src/index.html"
-      })
     ]
-  };
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    port: process.env.PORT || 3000
+  }
 };
