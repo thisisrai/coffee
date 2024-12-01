706c65d8b218cab41e99f02fa16e755eed983de0
Kavin Tjhan <tjhan.kavin@gmail.com>
2024-11-30 20:39:20 -0800

    fix: semi-fixed router

--- webpack.config.js
+++ webpack.config.js
@@ -3,82 +3,47 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env) => {
-  const production = env.production === "true";
+  const isProduction = env.production === true;
 
-  const main = {
-    mode: "production",
+  return {
     entry: "./src/index.jsx",
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "build"),
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[contenthash].js",
+      publicPath: "/"
     },
+    mode: isProduction ? "production" : "development",
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
-          },
+            loader: "babel-loader"
+          }
         },
         {
-          test: /\.m?\js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                ["@babel/plugin-proposal-class-properties", { loose: true }],
-                "@babel/plugin-transform-runtime",
-              ],
-            },
-          },
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(mp4|webm|ogg)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'videos',
-            },
-          },
-        },
-      ],
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      port: process.env.PORT || 3000,
+      static: {
+        directory: path.join(__dirname, "public")
+      }
     },
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        title: "My React Project",
-        template: "./src/index.html",
-      }),
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: "all",
-      },
-    },
+        template: "./src/index.html"
+      })
+    ]
   };
-
-  const dev = {
-    mode: "development",
-    devtool: "inline-source-map",
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, "dev"),
-      },
-      port: process.env.PORT,
-    },
-  };
-
-  return production ? main : { ...main, ...dev };
 };
